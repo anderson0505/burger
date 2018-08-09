a3c5cc951a440734c2d46f5d221aec6d752b448b
Mona Lisa <blane.anderson0505@gmail.com>
2018-08-09 09:52:53 -0700

    adding final files

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Veggie');
+INSERT INTO burgers (burger_name) VALUES ('Tofu');
+INSERT INTO burgers (burger_name) VALUES ('Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Bacon');
+INSERT INTO burgers (burger_name) VALUES ('Avocado');
+INSERT INTO burgers (burger_name) VALUES ('Couch');
